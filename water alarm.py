44aad5fdb4b89a6a36d7aa004a495aca37a143cf
HappyChuha <gaarav559@gmail.com>
2022-01-30 21:55:23 +0530

    my first prog

--- water alarm.py
+++ water alarm.py
@@ -4,13 +4,10 @@ mixer.init()
 mixer.music.load("alarm.mp3")
 mixer.music.set_volume(0.1)
 while True:
-    sleep(1)
+    sleep(1800)
     mixer.music.play(999999999)
     if mixer.music.get_busy:
         print("Enter yes if drank water")
         inp = input()
         if inp == "yes":
             mixer.music.stop()
-        else:
-            print("Enter yes you probably capitalised the letters")
-            continue
