687526f628484c2c26db36eea72af3d6133389b1
HappyChuha <gaarav559@gmail.com>
2022-02-01 09:37:44 +0530

    my first prog

--- song player.py
+++ song player.py
@@ -1,7 +1,7 @@
 from pygame import mixer
 mixer.init()
 mixer.music.load("lolz.mp3")
-mixer.music.set_volume(0.05)
+mixer.music.set_volume(0.1)
 mixer.music.play()
 while True:
     print("Enter p for pause, r for resume, t for checking time elapsed, s for setting position and e for exiting")
@@ -13,14 +13,14 @@ while True:
     elif inp == 'e':
         mixer.music.stop()
         mixer.music.unload()
-        for i in range(10):
-            print("bue")
         break
     elif inp == "t":
         timer = mixer.music.get_pos()
         print(timer/1000)
     elif inp == 's':
+        mixer.music.pause()
         setposinp = float(input("Enter time to set to: "))
         mixer.music.set_pos(setposinp)
+        mixer.music.unpause()
     else:
         print("Wrong input")
